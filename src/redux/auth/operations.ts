26f8004d9ef15c09ae5c9e016508e365ddeca957
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2025-02-21 22:46:45 +0000

    installed redux and axios dependencies, initialized redux persist and created state and reducer for authentication/authoriztion

---
+++ src/redux/auth/operations.ts
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+axios.defaults.baseURL = "https://connections-api.goit.global";
+
+const setAuthHeader = (token: string) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = "";
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post("/users/signup", credentials);
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error: any) {
+      if (axios.isAxiosError(error)) {
+        const errorMessage =
+          error.response?.data?.message ||
+          error.message ||
+          "Registration failed";
+        return thunkAPI.rejectWithValue(errorMessage);
+      } else {
+        return thunkAPI.rejectWithValue(
+          error.message || "An unexpected error occurred"
+        );
+      }
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post("/users/login", credentials);
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error: any) {
+      if (axios.isAxiosError(error)) {
+        const errorMessage =
+          error.response?.data?.message ||
+          error.message ||
+          "Registration failed";
+        return thunkAPI.rejectWithValue(errorMessage);
+      } else {
+        return thunkAPI.rejectWithValue(
+          error.message || "An unexpected error occurred"
+        );
+      }
+    }
+  }
+);
+
+export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+  try {
+    await axios.post("/users/logout");
+    clearAuthHeader();
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      const errorMessage =
+        error.response?.data?.message || error.message || "Registration failed";
+      return thunkAPI.rejectWithValue(errorMessage);
+    } else {
+      return thunkAPI.rejectWithValue(
+        error.message || "An unexpected error occurred"
+      );
+    }
+  }
+});
+
+export const refreshUser = createAsyncThunk(
+  "auth/refresh",
+  async (_, thunkAPI) => {
+    const state: RootState = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue("Unable to fetch user");
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const res = await axios.get("/users/current");
+      return res.data;
+    } catch (error: any) {
+      if (axios.isAxiosError(error)) {
+        const errorMessage =
+          error.response?.data?.message ||
+          error.message ||
+          "Registration failed";
+        return thunkAPI.rejectWithValue(errorMessage);
+      } else {
+        return thunkAPI.rejectWithValue(
+          error.message || "An unexpected error occurred"
+        );
+      }
+    }
+  }
+);
---
+++ src/redux/store.ts
@@ -0,0 +1,43 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { authReducer } from "./auth/authSlice";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// import { useDispatch, useSelector } from "react-redux";
+// import type { RootState, AppDispatch } from "./store";
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+// export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+// export const useAppSelector = useSelector.withTypes<RootState>();

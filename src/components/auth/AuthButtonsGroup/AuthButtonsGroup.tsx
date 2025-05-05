386dfb99acde8aeadbdade9e6c7988b018fb132c
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2025-05-05 23:49:37 +0100

    added logout

--- src/components/auth/AuthButtonsGroup/AuthButtonsGroup.tsx
+++ src/components/auth/AuthButtonsGroup/AuthButtonsGroup.tsx
@@ -1,11 +1,25 @@
 import css from "./AuthButtonsGroup.module.css";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useAppDispatch, useAppSelector } from "../../../redux/store";
+import { selectIsLoggedIn } from "../../../redux/auth/selectors";
+import { logOut } from "../../../redux/auth/operations";
 
 const AuthButtonsGroup: React.FC = () => {
   const { t } = useTranslation();
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const dispatch = useAppDispatch();
+  const handleClick = () => {
+    dispatch(logOut());
+  };
 
-  return (
+  return isLoggedIn ? (
+    <div className={css.authButtonsGroup}>
+      <button className={css.logout} onClick={handleClick}>
+        {t("logout.title")}
+      </button>
+    </div>
+  ) : (
     <div className={css.authButtonsGroup}>
       <Link className={css.login} to="/login">
         {t("login.signIn")}

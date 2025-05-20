ceea31dd2c2a8b6ab342702d39d7623f40c90c44
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2025-05-20 21:05:30 +0100

    added button for scrolling up the page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,43 @@
 import { useAppDispatch } from "../../redux/store";
 import HeroSection from "../../components/commonComponents/HeroSection/HeroSection";
 import RecommendedProductsSection from "../../components/products/RecommendedProductsSection/RecommendedProductsSection";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { refreshUser } from "../../redux/auth/operations";
 import { getCategories } from "../../redux/categories/operations";
 import { fetchProducts } from "../../redux/products/operations";
 import { getProductsQuery } from "../../helpers/fetchQuery";
+import ScrollToTopButton from "../../components/buttons/ScrollToTopButton/ScrollToTopButton";
 
 const HomePage = () => {
   const dispatch = useAppDispatch();
+  const [viewportHeight, setViewportHeight] = useState<number>(
+    window.innerHeight
+  );
+  const [scrolledFromTop, setScrolledFromTop] = useState<number>(
+    window.scrollY
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolledFromTop(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -20,6 +49,7 @@ const HomePage = () => {
     <div className="container">
       <HeroSection />
       <RecommendedProductsSection />
+      {scrolledFromTop > viewportHeight && <ScrollToTopButton />}
     </div>
   );
 };
